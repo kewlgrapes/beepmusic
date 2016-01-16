f5940dc3dcba8a49e75fd241c5689ed031bab18e
Nick Becker <becker.nick.a@gmail.com>
2016-01-16 01:07:28 -0600

    new file for web-based frontend

---
+++ beepserver.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+beepserver.py
+  Nick Becker
+  16 January, 2016
+  HTTP frontend for beepmusic program.
+"""
+import logging
+
+###########################################################
+# main program
+###########################################################
+
+if __name__ == "__main__":
+  pass
